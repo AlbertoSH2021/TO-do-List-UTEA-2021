b4c6499b6e99617b65fa3d970a45855d28fe997f
AlbertoSH2021 <201523437i@utea.edu.pe>
2021-11-28 16:07:30 -0500

    Login usuario y diseño de Interfaz

--- admin/InsertarUsuarios.php
+++ admin/InsertarUsuarios.php
@@ -5,12 +5,36 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Insertar datos</title>
-    <link rel="stylesheet" href="css/estilo.css">
+    
     <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </head>
+
 <body >
+<br>
+
+<div class="wrapper" id="contact">
+		<div class="wra">
+			
+			<center><h3>INICIAR SESION</h3></center>
+				    <div class="contact_form">
+						<!--  -->
+						<form action="../scripts/iniciar-sesion.php" method="POST">
+						  <span><input type="text"  placeholder="Usuario"  name="txtUsuario"  
+							 onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'NAME';}"
+							><label><img src="images/con1.png" alt="" /></label></span>									    
+						  <span class="left" ><input type="text"   placeholder="Contraseña"  name="txtClave" 
+						 onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'EMAIL';}"
+							><label><img src="images/con2.png" alt="" /></label></span>
+						  <div class="clear"></div>
+										    	
+						  <input type="submit" class="btn btn-primary " value="INICIAR SESION">
+						</form>
+						<!--  -->
+					</div>
+		</div>
+	</div>
 
 
         <div id="wrapper" class="container">
@@ -18,7 +42,7 @@
                 
                  <center>
 
-                    <h2>Registrar un Usuario Nuevo</h2><hr>
+                    <h2>REGISTRARTE</h2><hr>
                        <center><form  method="POST" name="form-work" action="logica/guardar.php">
                     
                                 <fieldset>
@@ -32,7 +56,7 @@
                                     <div class="form-group">
                                       <div class="col-md-6">
                                         <label class="control-label" for="nome">Contraseña</label>
-                                        <input name="clave" class="form-control" placeholder="Nombre usuario" type="text">
+                                        <input name="clave" class="form-control" placeholder="contraseña usuario" type="password">
                                         </div>
                                       </div>
                             
@@ -53,7 +77,7 @@
                                   
                                     <div class="form-group">
                                       <div class="col-md-6">
-                                        <button type="submit" class="btn btn-primary btn-lg btn-block info">Guardar</button>
+                                       <center><button type="submit" class="btn btn-primary wra">Registrarme</button></center> 
                                       </div>
                                     </div>     
                                 </fieldset> 
@@ -134,71 +158,71 @@
                 <div class="rectangulo"></div>
                 </section>
                 <style>
-                    body{
-                background: rgba(232, 235, 226, 0.568);
-                display: flex;
-                justify-content: center;
-                align-items: center;
-                min-height: 100vh;
-                flex-direction: column;
-                }
-                .button_atras{
-                    color: rgba(244, 241, 246, 0.808);
-                    text-decoration: none;
-                    font-size: 1.3rem;
-                    font-weight: bold;
-                    position: fixed;
-                    z-index: 10;
-                    top: 30px;
-                    right: 30px;
-                    background:rgb(236, 236, 236) ;
-                    height: 40px;
-                    width: 40px;
-                    cursor: pointer;
-                    border-radius: 10px;
-                    box-shadow: 0 0 6px rgba(0,0,0,1);
-                    
-                }
-                .button_refresh{
-                    color: rgba(241, 246, 241, 0.808);
-                    text-decoration: none;
-                    font-size: 1.3rem;
-                    font-weight: bold;
-                    position: fixed;
-                    z-index: 10;
-                    top: 30px;
-                    right: 90px;
-                    background:rgb(236, 236, 236) ;
-                    height: 40px;
-                    width: 40px;
-                    cursor: pointer;
-                    border-radius: 10px;
-                    box-shadow: 0 0 6px rgba(0,0,0,1);
-                    
-                }
-                .button_refresh0{
-                    color: rgba(241, 246, 241, 0.808);
-                    text-decoration: none;
-                    font-size: 1.3rem;
-                    font-weight: bold;
-                    position: fixed;
-                    z-index: 10;
-                    top: 30px;
-                    right: 150px;
-                    background:rgb(236, 236, 236) ;
-                    height: 40px;
-                    width: 40px;
-                    cursor: pointer;
-                    border-radius: 10px;
-                    box-shadow: 0 0 6px rgba(0,0,0,1);
-                    
-                }
+                                    body{
+                                background: rgba(232, 235, 226, 0.568);
+                                display: flex;
+                                justify-content: center;
+                                align-items: center;
+                                min-height: 100vh;
+                                flex-direction: column;
+                                }
+                                .button_atras{
+                                    color: rgba(244, 241, 246, 0.808);
+                                    text-decoration: none;
+                                    font-size: 1.3rem;
+                                    font-weight: bold;
+                                    position: fixed;
+                                    z-index: 10;
+                                    top: 30px;
+                                    right: 30px;
+                                    background:rgb(236, 236, 236) ;
+                                    height: 40px;
+                                    width: 40px;
+                                    cursor: pointer;
+                                    border-radius: 10px;
+                                    box-shadow: 0 0 6px rgba(0,0,0,1);
+                                    
+                                }
+                                .button_refresh{
+                                    color: rgba(241, 246, 241, 0.808);
+                                    text-decoration: none;
+                                    font-size: 1.3rem;
+                                    font-weight: bold;
+                                    position: fixed;
+                                    z-index: 10;
+                                    top: 30px;
+                                    right: 90px;
+                                    background:rgb(236, 236, 236) ;
+                                    height: 40px;
+                                    width: 40px;
+                                    cursor: pointer;
+                                    border-radius: 10px;
+                                    box-shadow: 0 0 6px rgba(0,0,0,1);
+                                    
+                                }
+                                .button_refresh0{
+                                    color: rgba(241, 246, 241, 0.808);
+                                    text-decoration: none;
+                                    font-size: 1.3rem;
+                                    font-weight: bold;
+                                    position: fixed;
+                                    z-index: 10;
+                                    top: 30px;
+                                    right: 150px;
+                                    background:rgb(236, 236, 236) ;
+                                    height: 40px;
+                                    width: 40px;
+                                    cursor: pointer;
+                                    border-radius: 10px;
+                                    box-shadow: 0 0 6px rgba(0,0,0,1);
+                                    
+                                }
                 </style>
                 
                     
-                <button type="radius" class="button_atras"><a href="index.php" class="back-button"><i class='bx bx-arrow-back'></i></a></button>
+                
                 <button type="radius" class="button_refresh"><a href="insertarusuarios.php" class="back-button"><i class='bx bx-refresh'></i></i></a></button>
-                <button type="radius" class="button_refresh0"><a href="insertarusuarios.php" class="back-button"><a href="../index.html"><i class='bx bx-power-off'></i></a></li> </a></button>
+                <button type="radius" class="button_atras" pha><a href="../index.html" class="back-button"><i class='bx bx-arrow-back'></i></a></button>
                 
             <!-- <li><a href="../scripts/cerrar-sesion.php">Cerrar sesión</a></li>  -->
             <!-- <li><a href="scripts/cerrar-sesion.php">Cerrar sesión</a></li> -->
@@ -209,37 +233,37 @@
 
 
         <style>
-          body{
-                background: rgba(232, 235, 226, 0.568);
-                display: flex;
-                justify-content: center;
-                align-items: center;
-                min-height: 100vh;
-                flex-direction: column;
-     }
-    body{
-                    height: 100vh;
-                    background-image: linear-gradient(to top, hsla(66, 83%, 9%, 0.582) 0%, rgba(175, 145, 63, 0.596) 100%),url(gift.gif);
-                    background-repeat: no-repeat;
-                    background-size: cover;
-                    background-attachment: fixed;
-                    background-position: center;
-                    color: White;
-                    font-size:150%;
-                    font-weight: bold;
+                        body{
+                               
+                                display: flex;
+                                justify-content: center;
+                                align-items: center;
+                                min-height: 100vh;
+                                flex-direction: column;
                     }
-                    .sidebar{
-   background-color:  rgba(255, 255, 255, 0.541);
-  }
-  .container-fluid{
-    background-color: rgba(0, 0, 0, 0.527);
-    border-radius: 20px;
-}
-.table-striped{
-    color: black;
-    background-color: rgba(95, 158, 160, 0.63);
-}
-</style>
+                    body{
+                                    
+                                    background-image: linear-gradient(to top, hsla(0, 71%, 12%, 0.582) 0%, rgba(68, 17, 64, 0.185) 100%),url(gift.gif);
+                                    background-repeat: no-repeat;
+                                    background-size: cover;
+                                    background-attachment: fixed;
+                                    background-position: center;
+                                    color: White;
+                                    
+                                    font-weight: bold;
+                                    }
+                                    .sidebar{
+                background-color:  rgba(255, 255, 255, 0.541);
+                }
+                .container-fluid{
+                    background-color: rgba(0, 0, 0, 0.527);
+                    border-radius: 20px;
+                }
+                .table-striped{
+                    color: black;
+                    background-color: rgba(95, 158, 160, 0.63);
+                }
+        </style>
 </body>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
