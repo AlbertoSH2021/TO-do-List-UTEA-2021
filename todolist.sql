5851f3094ec54be9299cb19b32dda145a2b5c502
AlbertoSH2021 <201523437i@utea.edu.pe>
2021-11-28 18:18:48 -0500

    creacion de abse de datos tabla listatareas

--- todolist.sql
+++ todolist.sql
@@ -14,3 +14,18 @@ DROP DATABASE IF EXISTS intranet;
 			('UsuarioPrueba', '1234', 0),
 			('Admin', '1234', 1);
 
+
+CREATE TABLE IF NOT EXISTS listaTareas (
+  cod_alumno int NOT NULL,
+  usuario varchar(50) NOT NULL,
+  asunto varchar(30) NOT NULL,
+  descripcion varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+ALTER TABLE listaTareas ADD PRIMARY KEY (cod_alumno);
+
+ALTER TABLE listaTareas MODIFY cod_alumno int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=100001;
+  
+INSERT INTO listaTareas (cod_alumno, usuario, asunto,descripcion) VALUES
+(100001, 'Alberto Sanchez', 'Programacion Movil', 'primer examne parcial el miercoles 01 diciembre '),
+(100002, 'Juancarlos Flores', 'Ingienieria de Software', 'Proyecto to do list en php');
